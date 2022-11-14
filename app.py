5af6992428b7fb7919b780ea7cf210a4ce6edbf1
OGBobston <dolgihandrey@gmail.com>
2022-11-14 15:38:32 +0600

    1

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 #!flask/bin/python
 from flask import Flask
+import os, subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/users/list', methods=['GET'])
+def getUsers():
+    output = subprocess.check_output([r'cd /usr/local/openvpn_as/scripts/ && ./sacli UserPropGet'], shell=True)
+    return jsonify({'users': output})
 
 if __name__ == '__main__':
     app.run(debug=True)
