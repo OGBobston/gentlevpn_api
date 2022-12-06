742e226daa3efc84a89dea18bf2f3cdbdf62be54
OGBobston <dolgihandrey@gmail.com>
2022-12-06 06:10:40 +0600

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -3,8 +3,8 @@ from models import *
 
 if __name__ == '__main__':
     try:
-    	if not users.is_closed():
-    		users.close()
+        if not users.is_closed():
+            users.close()
         User.create_table()
         print("ok")
     except Exception as e:
